d65a81d485a65c595a2c7a402784a01873926e47
Evgeny Golovin <yokopoomy20@gmail.com>
2023-10-26 11:12:35 +0300

    Better solution

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,18 @@
 export default function getSpecialAttack(object) {
-  if ('special' in object) {
-    const { special } = object;
-    if (special.length === 0) {
-      return 'Нет спец. атак';
-    }
-    for (let i = 0; i < special.length; i += 1) {
-      if (!('description' in special[i])) {
-        special[i].description = 'Описание недоступно';
-      }
-    }
-    return special;
+  const { special } = object;
+
+  if (!special) {
+    return 'Отсутствует поле special';
   }
-  return 'Отсутствует поле special';
+
+  if (special.length === 0) {
+    return 'Нет спец. атак';
+  }
+
+  const enhancedSpecial = special.map((item) => ({
+    ...item,
+    description: item.description || 'Описание недоступно',
+  }));
+
+  return enhancedSpecial;
 }
